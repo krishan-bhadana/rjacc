5b6f2a34ccf47d2229b0b7689642ad8e5baf82ba
krishan bhadana <bhadana.k11@gmail.com>
2017-07-17 00:03:29 +0530

    started using tkinter

---
+++ rj.py
@@ -0,0 +1,11 @@
+from tkinter import *
+
+#customer_details        # this carry the details of the customer
+#purchase_history        # this stores the purchase history of the customer
+
+root = Tk()
+root.geometry("1600x800")
+root.title("Ramu Jewellers")
+Tops = Frame(root, width = 1600, bg = "powder blue", relief = SUNKEN)
+Tops.pack(side = TOP)
+root.mainloop()
